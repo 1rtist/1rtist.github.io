2a86f2b8b14f7dabbfb9377630a93e6986c0c23d
1rtist <stealthetrigger@gmail.com>
2025-04-18 22:46:34 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ function resetGame() {
 
   // Reset the player's position
   player.style.left = "50px"; // Consistent X-axis position
-  player.style.bottom = "0px"; // Align with the bottom of the game area
+ 
 
   // Ensure the player is idle (not running or jumping)
   player.classList.remove("running", "jump", "jumping");
